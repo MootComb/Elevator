53458e1b09074d84909525a85a9c9191e8b84074
MootComb <76607017+MootComb@users.noreply.github.com>
2026-06-16 18:04:03 +0500

    Plugin update

--- src/main/java/com/MootComb/Elevator/Main.java
+++ src/main/java/com/MootComb/Elevator/Main.java
@@ -120,7 +120,7 @@ public final class Main extends JavaPlugin implements Listener {
         }
 
         getLogger().info("==================================================");
-        getLogger().info("   Elevator Enabled!");
+        getLogger().info("   Elevator Plugin Enabled!");
         getLogger().info("   Elevator blocks: " + elevatorBlocks.size());
         getLogger().info("   Teleporter blocks: " + teleporterBlocks.size());
         getLogger().info("   Max distance: " + blockDistance);
